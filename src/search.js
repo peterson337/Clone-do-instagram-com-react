3fd0a95b02bc19c986173e7ef9e73f3864cb65d2
peterson337 <73541319+peterson337@users.noreply.github.com>
2023-04-10 20:40:55 -0300

    Delete search.js

--- src/search.js
+++
@@ -1,8 +0,0 @@
-import {useEffect, useState} from 'react';
-import {auth} from './Firebase.js';
-
-export default function Search(value, onChange){
-    <input type="search" placeholder='Pesquise...'></input>
-
-
-}
